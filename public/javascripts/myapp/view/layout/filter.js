93cac3040d7442da6bc4747d69fc60ff88e0102a
tjl <tjl@example.com>
2017-05-25 17:24:15 +0800

    Added my filters

--- public/javascripts/myapp/view/layout/filter.js
+++ public/javascripts/myapp/view/layout/filter.js
@@ -6,25 +6,64 @@ timeFieldsPanel = new Ext.form.FieldSet({
     autoHeight:true,
     defaults: {width: '100%'},
     defaultType: 'datefield',
+    collapsible: true,
     items: [
-    	{name: 'fromDate', id: 'tfp_fromDateId', fieldLabel: 'From Date:'},
-    	{name: 'toDate', id: 'tfp_toDateId', fieldLabel: 'To Date:'}
+    	{
+    		name: 'fromDate',
+			id: 'tfp_fromDateId',
+			fieldLabel: 'From Date:',
+			value: new Date(new Date() - 3600 * 24 * 1000),
+			maxValue: new Date(),
+			format: 'Y-m-d',
+		},
+    	{
+    		name: 'toDate',
+			id: 'tfp_toDateId',
+			fieldLabel: 'To Date:',
+			value: new Date(),
+			maxValue: new Date(),
+			format: 'Y-m-d',
+		},
     ]
 });
-        
+
+var uas = Ext.create('Ext.data.Store', {
+    fields: ['name'],
+    data : [
+        {"name":"ua1"},
+        {"name":"ua2"},
+    ]
+});
+
 advancedFieldsPanel = new Ext.form.FieldSet({
     id: 'advancedFieldsPanelId',
     xtype:'fieldset',
     title: 'Advanced Filters',
     collapsible: false,
     autoHeight:true,
+    collapsed: false,
+    checkboxToggle: true,
     defaults: {width: '100%'},
     defaultType: 'textfield',
     items: [
-    	{ id: 'afp_platfromId', fieldLabel: 'Platform:'},
-		{ xtype: 'textfield', id: 'afp_uaId', fieldLabel: 'User Agent:'},
-		{ id: 'afp_hcdnVersionId', fieldLabel: 'HCDN Version:'}
-	]
+    	{ id: 'afp_platfromId', fieldLabel: 'Platform:', value: 'All', },
+		{ id: 'afp_hcdnVersionId', fieldLabel: 'HCDN Version:', value: 'All', },
+		{
+			xtype: 'combo',
+			id: 'afp_uaId',
+			fieldLabel: 'User Agent:',
+			store: uas,
+			queryMode: 'local',
+			displayField: 'name',
+			valueField: 'name',
+			value: 'All',
+		},
+	],
+    onAddNewField : function(btn){
+    alert('hi');
+        var form = btn.up(); // this is a better approach
+        form.add({xtype:'textfield', fieldLabel:"First Name"});
+    }
 });
 
 Ext.define('Demo.view.layout.filter',{
@@ -37,6 +76,7 @@ Ext.define('Demo.view.layout.filter',{
 	    region:'east',
 	    collapsible : true,
 	    containerScroll : true,
+	    autoScroll: true,
 	    split: true,
 	    margins : '0 0 -1 1',
 	    //cmargins: '3 3 3 3',
@@ -49,12 +89,18 @@ Ext.define('Demo.view.layout.filter',{
 	    },
 	    items: [timeFieldsPanel, advancedFieldsPanel],
 	    buttons: [
+	    	{
+	    		text: 'Add Filter',
+	    		handler: function() {
+	    			Ext.ComponentMgr.get('advancedFieldsPanelId').add({xtype:'textfield', fieldLabel:"First Name"});
+	    		},
+	    	},
 		    {
 		        text: 'Apply',
 		        id: 'fp_applyId',
 		        disabled : false
-		    }
-		    , {
+		    },
+		    {
 		        text: 'Reset',
 		        id: 'fp_resetId',
 		        disabled : false
@@ -63,7 +109,4 @@ Ext.define('Demo.view.layout.filter',{
     });
     this.callParent(arguments);
   }
-})
-
-var fd = Ext.ComponentMgr.get('tfp_toDateId');
-fd.setValue(Ext.Date.clearTime(new Date()));
+});
