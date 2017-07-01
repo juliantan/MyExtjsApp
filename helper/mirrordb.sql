68094d4148b905a1ac10bbb4abcec2a32a8a793c
tjl <tjl@example.com>
2017-07-01 23:38:29 +0800

    Updated schema for tbl_conf_report

--- helper/mirrordb.sql
+++ helper/mirrordb.sql
@@ -57,7 +57,7 @@ DROP TABLE IF EXISTS `tbl_conf_report`;
 CREATE TABLE `tbl_conf_report` (
   `ID` bigint(20) NOT NULL AUTO_INCREMENT,
   `Title` varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `TableName` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `TableName` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
   `ParentId` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
